b3c47a048d414cb368f24eaffb7bcf97d61198b1
Hai (James) <mai.son.hai@sun-asterisk.com>
2025-11-17 17:33:41 +0700

    init

--- main.cpp
+++ main.cpp
@@ -2,4 +2,31 @@
 // Created by mai.son.hai@sun-asterisk.com on 17/11/2025.
 //
 
-#include "main.h"
+#include <iostream>
+
+using namespace std;
+
+// --- Prepare ADT ---
+
+// Priority Queue with Set
+
+// Weighted Graph
+//TODO: I'm considering how to represent a Weighted Graph: Node list, Edge list, Adj List?
+
+// Dijsktra
+
+int main(){
+    freopen("../input.txt", "r", stdin);
+
+    int nTestCases;
+    cin >> nTestCases;
+
+    while (nTestCases--) {
+
+    }
+
+
+
+
+
+}
